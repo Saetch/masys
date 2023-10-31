beb23c8204b5f58b5795debd70ada932e429fad4
wriedtn <nils.wriedt@stud.th-luebeck.de>
2023-10-31 02:28:50 +0100

    added simulation for different agents

--- src/main/java/nilsoscar/App.java
+++ src/main/java/nilsoscar/App.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nilsoscar.agents.Agent;
+import nilsoscar.environment.EnvData;
+import nilsoscar.environment.Environment;
+import nilsoscar.environment.Situation;
 /**
  * Hello world!
  *
@@ -11,15 +14,45 @@ import nilsoscar.agents.Agent;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {   
 
-        World world = new World();
-        world.agents = new ArrayList<Agent>();
-        world.agents.add(new Agent());
+        final int MAX_DRAWS_PER_WORLD = 200;
+        int c = 0;
+
+        //this is just stuff I added to give better output
+        List<Double>[] results = new ArrayList[MAX_DRAWS_PER_WORLD];
+        for (int i = 0; i < MAX_DRAWS_PER_WORLD; i++){
+            results[i] = new ArrayList<Double>();
+        }
+        do{
+            System.out.println("Setting up new world number: "+c+"\n");
+
+            World world = new World();
+            world.agents = new ArrayList<Agent>();
+            world.agents.add(new Agent());
+            Environment environment = new Environment(3.5, 5.0, 6.3, 4.0);
+            world.environment = environment;
+
+            //I give every test run about X draws, so that the agent has a chance to get a good idea of the situation, then reset
+            for(int i = 0; i < MAX_DRAWS_PER_WORLD; i++){
+                results[i].add(world.testerRun(0));
+            }
 
 
-        
-        System.out.println( "Hello World!" );
+
+            c++;
+            if(c % 10 == 0){
+                System.out.println("Average correct guesses after "+c+" worlds:");
+                for(int i = 0; i < MAX_DRAWS_PER_WORLD; i++){
+                    Double sum = 0.0;
+                    for(Double d : results[i]){
+                        sum += d;
+                    }
+                    System.out.println("After "+i+" draws: "+sum/c);
+                }
+            }
+        }
+        while (true);
 
 
     }
@@ -30,6 +63,40 @@ public class App
 
 class World{
     List<Agent> agents;
+    Environment environment;
+
+    public World() {
+    }
+
+    public World(List<Agent> agents) {
+        this.agents = agents;
+    }
+
+    public Double testerRun(int index){
+        final int RUNS  = 1_000;
+        int correct_guesses = 0;
+        Double corret_val;
+
+        //I want to check how many times the agent guesses correctly in a certain number of guesses, before I add more information
+        for (int i = 0; i < RUNS; i++){
+            EnvData data = environment.nextValue();
+            Situation guess = this.agents.get(index).decideOn(data);
+
+            if(guess == data.reality()){
+                correct_guesses++;
+            }
+
+        }
+        corret_val = (double) correct_guesses / RUNS;
+        
+        //System.out.println("Correct guesses: " + corret_val+" with knowledge of: "+this.agents.get(index).getDangerCount()+" dangerous and "+this.agents.get(index).getHarmlessCount()+" harmless measurements");
+        EnvData data_to_add = environment.nextValue();
+        this.agents.get(index).addMeasurement(data_to_add);
+        
+        return corret_val;
+    }
+
+
 
 }
 
--- src/main/java/nilsoscar/agents/Agent.java
+++ src/main/java/nilsoscar/agents/Agent.java
@@ -10,7 +10,6 @@ import nilsoscar.environment.Situation;
 
 public class Agent {
     private List<Double>[] measurements;
-    public Double neighborhoodSize = 0.1;
     public int minimumRequiredMatches = 3;
 
 
@@ -18,87 +17,107 @@ public class Agent {
         this.measurements = new ArrayList[2];
     }
 
-    public Situation decideOn(EnvData envData) {
-        if(measurements[0] == null){
-            measurements[0] = new ArrayList<Double>();
-            //return a random decision, as we don't have enough data yet
-            return Situation.values()[(int) (Math.random() * Situation.values().length)];
+    public int getHarmlessCount(){
+        if (measurements[0] == null){
+            return 0;
         }
-        if(measurements[1] == null){
-            measurements[1] = new ArrayList<Double>();
-            //return a random decision, as we don't have enough data yet
-            return Situation.values()[(int) (Math.random() * Situation.values().length)];
+        return measurements[0].size();
+    }
+
+    public int getDangerCount(){
+        if (measurements[1] == null){
+            return 0;
+        }
+        return measurements[1].size();
+    }
+
+    public Situation decideOn(EnvData envData) {
+
+        for ( List<Double> meas : measurements){
+            if(meas == null){
+                //if there is no data to go by, just guess
+                return Situation.values()[(int) (Math.random() * Situation.values().length)];
+            }
         }
 
-        //this is used to just broadly set up the values that need to be checked for t 
+
+        
         Double harmless_mean = measurements[0].stream().mapToDouble(a -> a).average().getAsDouble();
         Double danger_mean = measurements[1].stream().mapToDouble(a -> a).average().getAsDouble();
-        
 
         //Vorwissen laut Aufgabenstellung, mittelwert für gefährlich > ungefährlich, also wenn nicht der Fall, dann keine brauchbaren Daten vorhanden
-        if(harmless_mean <= danger_mean ){
+        if(harmless_mean >= danger_mean ){
             return Situation.values()[(int) (Math.random() * Situation.values().length)];
         }
 
 
         //if it is outside of the contested range, we should be able to guess the more likely solution
-        if(envData.measurement() < harmless_mean){
+        if(envData.measurement() <= harmless_mean){
             return Situation.HARMLESS;
         }
-        if(envData.measurement() > danger_mean){
+        if(envData.measurement() >= danger_mean){
             return Situation.DANGER;
         }
 
         //use successive approximation on the own data for finding the right t? Idk, I'm just doing it
-        Situation retVal = this.successiveApproximation(envData, harmless_mean, danger_mean);
+        Situation retVal = this.checkDistToClosestNeighbors(envData.measurement());
+
 
 
         return retVal;
     }
 
 
-    private Situation successiveApproximation(EnvData envData, Double harmless_mean, Double danger_mean){
-        Double lowerLimit = harmless_mean;
-        Double upperLimit = danger_mean;
-        Double check_range = this.neighborhoodSize;
-        do{
-            Double upper_hits = this.checkForSamplesInRange(upperLimit, check_range, Situation.DANGER);
-            Double lower_hits = this.checkForSamplesInRange(lowerLimit, check_range, Situation.HARMLESS);
-        }
-        while(upperLimit - lowerLimit > neighborhoodSize);
+    private Situation checkDistToClosestNeighbors(Double point){
 
+        int len = measurements[0].size() < measurements[1].size() ? measurements[0].size() : measurements[1].size();
+
+        //in order to make this fair, we need to limit the stream to the length of the shorter list, otherwise the other one would likely get more or better hits. This should not matter for big enough lists
+        Stream<Double> harmlessStream = measurements[0].stream().limit(len);
+        Stream<Double> dangerStream = measurements[1].stream().limit(len);
+        int numberOfNeighbors = this.minimumRequiredMatches > len ? len : this.minimumRequiredMatches;
+
+        //find the closest neighbors 
+        DoubleStream harmlessNeighbors = harmlessStream.mapToDouble(m -> Math.abs(m - point)).sorted().limit(numberOfNeighbors);
+        DoubleStream dangerNeighbors = dangerStream.mapToDouble(m -> Math.abs(m - point)).sorted().limit(numberOfNeighbors);
+    
+        //get the average distance to the neighbors
+        double harmlessAvg = harmlessNeighbors.average().getAsDouble();
+        double dangerAvg = dangerNeighbors.average().getAsDouble();
+        
+        //if the distance to the harmless neighbors is smaller, it is more likely to be harmless
+        if(harmlessAvg < dangerAvg){
+            return Situation.HARMLESS;
+        }
+        return Situation.DANGER;
+    
     }
 
-    //TODO! This needs rewriting and a more sophisticated approach
-    private Double checkForSamplesInRange(Double point, Double range, Situation s){
-        Double retVal = 0.0;
-        Stream<Double> str;
-        Double range_f = Math.abs(range);
-        if(s == Situation.HARMLESS){
-            str = measurements[0].stream();
-        }
-        else{
-            str = measurements[1].stream();
-        }
-        //check for every value if it is in between the point and point + range
-        DoubleStream stream = str.mapToDouble( a -> a).filter( a ->  a >= point-range_f && a <= point+range_f );
-        retVal += stream.count();
 
-        if (retVal >= this.minimumRequiredMatches){
-            return retVal;
+    public int addMeasurement(EnvData envData) {
+        int index = envData.reality().ordinal();
+        if(measurements[index] == null){
+            measurements[index] = new ArrayList<Double>();
+        }
+        measurements[index].add(envData.measurement());
+
+        //check if any of these is null
+        if(measurements[0] == null || measurements[1] == null){
+            return 0;
+        }
+        int lower_len = measurements[0].size() < measurements[1].size() ? measurements[0].size() : measurements[1].size();
+
+        //I chose an arbitrary rule to set the number of required matches, in this case y = 2x² for a more sophisticated approach, this should propably be tested for what works best
+        if( lower_len > 2*Math.pow(this.minimumRequiredMatches, 2)){
+            this.minimumRequiredMatches++;
         }
 
-        //if we dont have enough hits, we check the closest neighbors and see how far they are.
-        int forcedMaximum = Math.min(this.measurements[0].size(), this.measurements[1].size());  //the smallest set is the maximum we can check
-        int req_hits = Math.min(this.minimumRequiredMatches, forcedMaximum); //try to get minimum required matches, but if we have less data, we need to lower the requirement
-        
-        //check the closest neighbors
-        DoubleStream stream2 = str.mapToDouble( a -> Math.abs(a - point)).sorted().limit(req_hits);
-        retVal += stream2.max().getAsDouble();
-        if (retVal >= this.minimumRequiredMatches){
-            return retVal;
-        }
-        
+        return lower_len;
     }
+
+  
+    
 
 }
+
+record DoubleTuple(double harmless, double danger){};
